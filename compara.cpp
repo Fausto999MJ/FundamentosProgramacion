3a780f2ae73c867710b82dd82c917478ae84cb84
Faustomj03 <fausto.montano.jaen@utelvt.edu.ec>
2022-09-23 22:27:32 -0500

    Modificacion del programa compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,23 @@
+
+//Programa creado por Fausto Montaño
+//curso: 1ro "B" TICS
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float f,s;
+	cout<<"ingrese f s:"; cin>>f>>s;
+        if(f>s)
+	{
+		cout<<f<<"es mayor s "<<s<<endl;
+	}
+	else{
+		cout<<s<<" es mayor f"<<f<<endl;
+	}
+
+	return 0;
+}
+
+//2022
+
